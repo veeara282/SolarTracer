25d219c3e4243376fad1dec68178bf7469613612
Evelyn Fitzgerald <acf67@cornell.edu>
2020-12-23 22:06:21 -0500

    Write basic forward() algorithm

--- efficientnet_pytorch/segmentation.py
+++ efficientnet_pytorch/segmentation.py
@@ -15,6 +15,25 @@ class EfficientNetSegmentation(nn.Module):
             self.endpoint = f'reduction_{endpoint}'
         else:
             raise ValueError('endpoint must be between 1 and 5, inclusive')
-    
-    def forward(self, batch):
-        pass
+        # Create some number of Conv2d's followed by an AvgPool2d and Linear
+        self.conv2ds = []
+        # TODO Extract channel count from .utils module
+        # Also, could use depthwise separable convolution instead (see MobileNet paper) --
+        # it's what EfficientNet uses
+        self.conv2ds.append(nn.Conv2d(16, 16, 3))
+        self.avgpool = nn.AvgPool2d()
+        self.linear = nn.Linear(16, 2)
+
+    def forward(self, inputs, return_cam=False):
+        # Extract hidden state from middle of EfficientNet
+        hidden_state = self.backbone.extract_endpoints(inputs)[self.endpoint]
+        # Apply Conv2d layers in succession
+        for conv2d in self.conv2ds:
+            hidden_state = conv2d(hidden_state)
+        # Return either scores or activation map
+        if return_cam:
+            class_activation_map = self.linear(hidden_state)
+            return class_activation_map
+        else:
+            class_scores = self.linear(self.avgpool(hidden_state))
+            return class_scores
