ad073f6c107f54a439139b76a54628a3d3e4cd26
Evelyn Fitzgerald <acf67@cornell.edu>
2020-12-23 21:21:22 -0500

    Write skeleton code for segmentation module

--- efficientnet_pytorch/segmentation.py
+++ efficientnet_pytorch/segmentation.py
@@ -1,1 +1,20 @@
 # TODO Implement CAM-GLWT for EfficientNet
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+from .model import EfficientNet
+
+class EfficientNetSegmentation(nn.Module):
+    def __init__(self, backbone='efficientnet-b0', endpoint=1):
+        super().__init__()
+        # Use EfficientNet classifier as a backbone
+        self.backbone = EfficientNet.from_pretrained(backbone)
+        # Use endpoint from reduction level i in [1, 2, 3, 4, 5]
+        if endpoint in range(1, 6):
+            self.endpoint = f'reduction_{endpoint}'
+        else:
+            raise ValueError('endpoint must be between 1 and 5, inclusive')
+    
+    def forward(self, batch):
+        pass
