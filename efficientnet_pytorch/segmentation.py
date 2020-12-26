0ef859fa9f33b535dc03d14d2a61145323597d22
Evelyn Fitzgerald <acf67@cornell.edu>
2020-12-26 15:19:58 -0500

    Add basic training routines Also split freeze_layers into two methods

--- efficientnet_pytorch/segmentation.py
+++ efficientnet_pytorch/segmentation.py
@@ -2,6 +2,9 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
+from torch.utils.data.dataloader import DataLoader
+from torch.optim import Optimizer
+from tqdm import tqdm, trange
 
 from .model import EfficientNet
 
@@ -23,6 +26,7 @@ class EfficientNetSegmentation(nn.Module):
         self.conv2ds.append(nn.Conv2d(16, 16, 3))
         self.avgpool = nn.AvgPool2d()
         self.linear = nn.Linear(16, 2)
+        # Loss function
 
     def forward(self, inputs, return_cam=False):
         # Extract hidden state from middle of EfficientNet
@@ -38,9 +42,12 @@ class EfficientNetSegmentation(nn.Module):
             class_scores = self.linear(self.avgpool(hidden_state))
             return class_scores
     
-    def freeze_layers(self):
-        '''Freezes the backbone and all current Conv2d layers.'''
+    def freeze_backbone(self):
+        '''Freezes the backbone.'''
         self.backbone.requires_grad_(False)
+
+    def freeze_conv2d_layers(self):
+        '''Freezes all current Conv2d layers.'''
         for conv2d in self.conv2ds:
             conv2d.requires_grad_(False)
 
@@ -48,3 +55,64 @@ class EfficientNetSegmentation(nn.Module):
         '''Adds a new Conv2d layer and resets the Linear layer.'''
         self.conv2ds.append(nn.Conv2d(16, 16, 3))
         self.linear.reset_parameters()
+
+def to_device(obj):
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+    return obj.to(device)
+
+def train_or_eval(model: nn.Module,
+                  data_loader: DataLoader,
+                  loss_criterion: nn.Module,
+                  optimizer: Optimizer,
+                  train: bool = False):
+    if train:
+        model.train()
+    else:
+        model.eval()
+    total = 0
+    total_loss = 0
+    correct = 0
+    for batch in tqdm(data_loader, leave=False, desc=("Training Batches" if train else "Validation Batches")):
+        output = model(batch)
+        total += output.size()[0]
+        predicted = torch.argmax(output, 1).cpu()
+        # Labels are stored in the batch dict
+        expected_out = batch['answerable']
+        correct += (expected_out == predicted).numpy().sum()
+        if train:
+            optimizer.zero_grad() 
+            loss = loss_criterion(output, to_device(expected_out))
+            total_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+        else:
+            total_loss += loss_criterion(output, to_device(expected_out)).item()
+    total_loss /= total
+    # Print accuracy
+    print('Training Results:' if train else 'Validation Results:')
+    print(f'Loss: {total_loss:.4f}')
+    print(f'Correct: {correct} ({correct / total:.2%})')
+    print(f'Total: {total}\n')
+
+
+def train_segmentation(model: EfficientNetSegmentation,
+                       train_loader: DataLoader,
+                       val_loader: DataLoader,
+                       loss_criterion: nn.Module,
+                       optimizer: Optimizer):
+    # Train the backbone
+    num_epochs = 5
+    for epoch in trange(num_epochs, desc='Train backbone'):
+        train_or_eval(model.backbone, train_loader, loss_criterion, optimizer, train=True)
+        train_or_eval(model.backbone, val_loader, loss_criterion, optimizer)
+    # Train the segmentation branch
+    model.freeze_backbone()
+    for epoch in trange(num_epochs, desc='Train segmentation branch'):
+        train_or_eval(model, train_loader, loss_criterion, optimizer, train=True)
+        train_or_eval(model, train_loader, loss_criterion, optimizer)
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
